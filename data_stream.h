d8a342486ce23e2f336278e746158c2fb2124b4a
eyup.gurel <eyup.gurel@futurance.com>
2022-01-15 21:35:41 +0300

    added files

---
+++ data_stream.h
@@ -0,0 +1,25 @@
+//
+// Created by egl on 14.01.2022.
+//
+
+#ifndef MATCHING_ENGINE_TEST_UTILS_DATA_STREAM_H
+#define MATCHING_ENGINE_TEST_UTILS_DATA_STREAM_H
+
+
+#include <random>
+#include <chrono>
+#include <matching_engine.h>
+
+using namespace std;
+using namespace std::chrono;
+
+void prepareOrderVector(int count, int order_type, double price, double quantity_dist_min, double quantity_dist_max,
+                        std::vector<order>& v_orders);
+
+void prepareOrderVector(int count, int order_type, double price_dist_min, double price_dist_max,
+                        double quantity_dist_min, double quantity_dist_max,
+                        std::vector<order>& v_orders);
+void prepareOrderSet (std::vector<order>& v_orders,
+                      TOrders& orders);
+
+#endif //MATCHING_ENGINE_TEST_UTILS_DATA_STREAM_H
