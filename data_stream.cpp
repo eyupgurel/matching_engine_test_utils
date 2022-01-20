2e9e05ebe4f91ba84d5d857a3b375449d753dd13
eyup.gurel <eyup.gurel@futurance.com>
2022-01-20 11:30:56 +0300

    refactored

--- data_stream.cpp
+++ data_stream.cpp
@@ -5,6 +5,7 @@
 #include "data_stream.h"
 
 static int counter = 0;
+static long offset = 549755813888;
 
 void prepareOrderVector(int count, int order_type, double price, double quantity_dist_min, double quantity_dist_max,
                         std::vector<order>& v_orders) {
@@ -17,7 +18,7 @@ void prepareOrderVector(int count, int order_type, double price, double quantity
         long epoch_milli = std::chrono::duration_cast<std::chrono::milliseconds>(
                 std::chrono::system_clock::now().time_since_epoch()
         ).count();
-        v_orders.emplace_back(price, epoch_milli, qd, ++counter, order_type);
+        v_orders.emplace_back(price, i%2 == 0 ? offset : epoch_milli , qd, ++counter, order_type);
     }
 }
 
@@ -30,14 +31,13 @@ void prepareOrderVector(int count, int order_type, double price_dist_min, double
     uniform_real_distribution<double> price_dist(price_dist_min, price_dist_max);
     uniform_real_distribution<double> quantity_dist(quantity_dist_min, quantity_dist_max);
 
-
-    for(int i =1; i<=count; i++){
+    for(int i =1; i<=count; i++) {
         auto pd = price_dist(gen);
         auto qd = quantity_dist(gen);
         long epoch_milli = std::chrono::duration_cast<std::chrono::milliseconds>(
                 std::chrono::system_clock::now().time_since_epoch()
         ).count();
-        v_orders.emplace_back(pd, epoch_milli, qd, ++counter, order_type);
+        v_orders.emplace_back(pd, i%2 == 0 ? offset : epoch_milli, qd, ++counter, order_type);
     }
 }
 
